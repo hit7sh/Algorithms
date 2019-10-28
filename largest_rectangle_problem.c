9968d16e021c1659bc52aaf235199d299267bb0b
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2019-10-28 23:05:22 +0530

    Update largest_rectangle_problem.c

--- largest_rectangle_problem.c
+++ largest_rectangle_problem.c
@@ -1,3 +1,15 @@
+/*
+A histogram is a polygon composed of a sequence of rectangles aligned at a common base line. 
+The rectangles have equal widths but may have different heights. 
+For example, the figure on the left shows the histogram that consists of rectangles with 
+the heights 2, 1, 4, 5, 1, 3, 3, measured in units where 1 is the width of the rectangles.
+The area of maximum rectangle is 4*2=8
+
+Output on a single line the area of the largest rectangle in the specified histogram. 
+Remember that this rectangle must be aligned at the common base line.
+*/
+
+
 #include<stdio.h>
 int main()
 {
