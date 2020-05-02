2e57be1655955fc26cf1351bdd0138aea3e27d28
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-05-03 00:27:49 +0530

    Update nCr_DP.cpp

--- nCr_DP.cpp
+++ nCr_DP.cpp
@@ -1,13 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 long long int dp[778][778]{0};
-void generate()
-{int i,j;
-    dp[2][1]=2;
-    for(i=2;i<=777;i++)for(j=0;j<=i;j++)if(j==0||j==i)dp[i][j]=1;
-    for(i=3;i<=777;i++)
-    for(j=1;j<i;j++)
-    dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
+void generate(){
+    int i,j;
+    for(i=1;i<=777;i++)
+        dp[i][i]=dp[i][0]=1;
+        
+    for(i=2;i<=777;i++)
+        for(j=1;j<i;j++)
+            dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
 }
 int main()
 {
