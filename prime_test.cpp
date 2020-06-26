91d43da0a2d9729333c6b34709aa56bac5b870c9
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-06-26 13:33:16 +0530

    Create prime_test.cpp

---
+++ prime_test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+usign ll=long long;
+
+bool prime(ll n) { 
+  if(n < 2) 
+    return 0; 
+  else if (n < 4) 
+    return true; 
+  if (n&1^1 || n % 3 == 0) 
+    return false; 
+  for (ll i = 5; i * i <= n; i = i + 6) 
+    if (n % i == 0 || n % (i + 2) == 0) 
+      return false; 
+  return true; 
+}
+int main()
+{
+  cout << prime(7LL);
+  return 0;
+}
