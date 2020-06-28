554d559eceb90fb259faa5ccd86315020b5c37bb
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-06-28 22:58:13 +0530

    Create divisors_count.cpp

---
+++ divisors_count.cpp
@@ -0,0 +1,16 @@
+int countDivisors(int n) 
+{ 
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) { 
+        if (n % i == 0) { 
+            // If divisors are equal, 
+            // count only one 
+            if (n / i == i) 
+                cnt++; 
+  
+            else // Otherwise count both 
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
