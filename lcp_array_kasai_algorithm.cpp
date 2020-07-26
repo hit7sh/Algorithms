faa9833125e5c90d40202c66c99616e5d4db2035
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-07-26 11:05:26 +0530

    Create lcp_array_kasai_algorithm.cpp

---
+++ lcp_array_kasai_algorithm.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e6+100;
+
+vector<int> kasai(string &s, vector<int> &sa)
+{
+    int n = s.size(), k = 0;
+    vector<int> ra(n), lcp(n);
+    for(int i = 0; i < n; i++) ra[sa[i]] = i;
+    for(int i = 0; i < n; i++)
+    {
+        if(k) k--;
+        if(ra[i] == n - 1) {k = 0; continue;}
+        int j = sa[ra[i] + 1];
+        while(k < n && s[(i + k) % n] == s[(j + k) % n]) k++;
+        lcp[ra[i]] = k;
+        if(ra[(sa[ra[i]] + 1) % n] > ra[(sa[ra[j]] + 1) % n]) k = 0;
+    }
+    return lcp;
+}
