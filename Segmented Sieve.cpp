971dc886ceafde55c44abc2f29da8a87c3f992ab
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-04-19 09:07:14 +0530

    Create Segmented Sieve.cpp

---
+++ Segmented Sieve.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int64_t> segmentedSieve(int64_t L, int64_t R) {
+    int64_t i, j;
+    vector<int64_t> P;
+    vector<bool> isPrime(R - L + 1, true);
+
+    for(i = 2; i * i <= R; i++){
+        int64_t j = max(i + i, ((L + i - 1) / i) * i);
+
+        for(; j <= R; j += i)
+            isPrime[j - L] = false;
+    }
+
+    for(i = L; i <= R; i++)
+        if(i - 1 && isPrime[i-L])
+            P.emplace_back(i);
+
+    return P;
+}
