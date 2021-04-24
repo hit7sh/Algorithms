be57a7f82666968295febe62e4587f2629d2ef1a
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-04-24 17:19:16 +0530

    Update SQF(prime power parity generator).cpp

--- SQF(prime power parity generator).cpp
+++ SQF(prime power parity generator).cpp
@@ -2,6 +2,12 @@ const int N = 1e7 + 5;
 
 vector<int> sqf(N + 1);
 void generateSqf() {
+  /*
+  * suppose n = 90
+  * prime factorization of 90 = 2^1 * 3^2 * 5^1
+  * In sqf representation we consider only the primes with 'odd' powers.
+  * so sqf(90) = 2 * 5 = 10
+  */
   for (int i = 0; i <= N; i++){
     sqf[i] = i;
   }
