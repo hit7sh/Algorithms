d983ed3faf92089c5f1dca6ef8339c0d71287bc8
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-09-10 12:58:48 +0530

    Update Manacher_vector.cpp

--- Manacher_vector.cpp
+++ Manacher_vector.cpp
@@ -1,6 +1,6 @@
-vvi manacher(string& s){
-	int n=s.size();
-	vector<int> d1(n);
+vector<vector<int>> manacher(string& s){
+  int n=s.size();
+  vector<int> d1(n);
 for (int i = 0, l = 0, r = -1; i < n; i++) {
     int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
     while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) {
@@ -11,18 +11,23 @@ for (int i = 0, l = 0, r = -1; i < n; i++) {
         l = i - k;
         r = i + k;
     }
-	}
-	vector<int> d2(n);
+  }
+  vector<int> d0(n);
 for (int i = 0, l = 0, r = -1; i < n; i++) {
-    int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
+    int k = (i > r) ? 0 : min(d0[l + r - i + 1], r - i + 1);
     while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) {
         k++;
     }
-    d2[i] = k--;
+    d0[i] = k--;
     if (i + k > r) {
         l = i - k - 1;
         r = i + k ;
     }
 }
-    return {d1, d2};
+  // d0 = even center, what is longest radius excluding center to the left
+  //      length of odd palindrome = 2 * d0[i]
+
+  // d1 = odd center, what is longest radius including center to the left
+  //      length of even palindrome = 1 + 2 * (d1[i] - 1)
+    return {d0, d1};
 }
