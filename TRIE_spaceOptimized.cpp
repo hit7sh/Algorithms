c68e32e554db6ccf0a381e99459607fe711f89cd
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-09-14 21:10:59 +0530

    Update TRIE_spaceOptimized.cpp

--- TRIE_spaceOptimized.cpp
+++ TRIE_spaceOptimized.cpp
@@ -1,44 +1,33 @@
-// Author :: <Hitesh_Saini>
-#include<bits/stdc++.h>
-using namespace std;
+class Trie {
+public:
+    struct trie{
+        unordered_map<char, trie*>M;
+        bool end;
+    } *root;
+    typedef trie node;
 
-struct trie{
-    unordered_map<char, trie*>M;
-    bool end;
-} *root;
-typedef trie node;
-
-node* newnode(){
-    node *temp = new node;
-    return (temp->end = false, temp);
-}
-
-void insert(string st){
-    node *t = root = (root == nullptr?newnode():root);
-    for(char x:st){
-        if(t->M.find(x)==t->M.end())
-            t->M[x] = newnode();
-        t = t->M[x];
+    node* newnode(){
+        node *temp = new node;
+        return (temp->end = false, temp);
     }
-    t->end = true;
-}
 
-bool search(string st){
-    node *t=root;
-    for(char x:st){
-        if(t==nullptr || t->M[x]==nullptr)
-            return false;
-        t = t->M[x];
+    void insert(string st){
+        node *t = root = (root == nullptr?newnode():root);
+        for(char x:st){
+            if(t->M.find(x)==t->M.end())
+                t->M[x] = newnode();
+            t = t->M[x];
+        }
+        t->end = true;
     }
-    return t->end;
-}
 
-int main(){
-    insert("dog");
-    insert("cat");
-    insert("dogos");
-    insert("cattie");
-    insert("catsdog");
-    cout<<boolalpha<<search("cattie");
-    return 0;
-}
+    bool search(string st){
+        node *t=root;
+        for(char x:st){
+            if(t==nullptr || t->M[x]==nullptr)
+                return false;
+            t = t->M[x];
+        }
+        return t->end;
+    }
+};
