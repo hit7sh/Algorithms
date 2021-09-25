0f7d85b140f8c38872726c66dcb0c656b21f348d
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-09-25 22:23:55 +0530

    Update Lis Generic.cpp

--- Lis Generic.cpp
+++ Lis Generic.cpp
@@ -1,26 +1,17 @@
 #include<bits/stc++.h>
 using namespace std;
 
-template<typename T>
-int lis(const vector<T>& A) {
-    int n = A.size();
-    T dp[n + 1];
-    memset(dp, 0x3f, sizeof(dp));
-    memset(dp, 0xc0, sizeof(T));
-    const T INF = dp[1];
-
-
-    // dp[i] = last element of increasing subsequence of length i
-    for (int i = 0; i < n; i++) {
-        int j = upper_bound(dp, dp + n + 1, A[i]) - dp;
-        if (dp[j-1] < A[i] && A[i] < dp[j])
-            dp[j] = A[i];
-    }
-
-    int ans = 0;
-    for (int i = 0; i <= n; i++) {
-        if (dp[i] < INF)
-            ans = i;
-    }
-    return ans;
+vector<int> lis(vector<int> a){
+	int n = (int)a.size();
+	vector<int> r(n, 0);
+	vector<int> stk;
+	for(int i = 0; i < n; i++){
+		int x = lower_bound(stk.begin(), stk.end(), a[i]+1) - stk.begin();
+		r[i] = x;
+		if(x >= (int)stk.size()){
+			stk.push_back(a[i]);
+		}
+		stk[x] = a[i];
+	}
+	return r;
 }
