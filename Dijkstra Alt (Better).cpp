32de64209cc93eb39e88f02504df5f5560543431
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-10-10 21:26:18 +0530

    Update Dijkstra Alt (Better).cpp

--- Dijkstra Alt (Better).cpp
+++ Dijkstra Alt (Better).cpp
@@ -1,44 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(vector<int>& v) { cout << v; }
-ostream& operator<<(ostream& ot, vector<int>& v) { for (int& x: v) ot << x << ' '; return ot; }
-
-void solve() {	
-	int m, n, u, v, w;
-	cin >> n >> m;
-	vector<vector<pair<int, int>>> adj(n);
-
-	while (m--) {
-		cin >> u >> v >> w;
-		--u, --v;
-		adj[u].emplace_back(v, w);
-		adj[v].emplace_back(u, w);
-	}
-
-	int src;
-	cin >> src;
-	--src;
-
-	vector<int> vis(n), dist(n, -1);
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	pq.emplace(0, src); // (dist, vertex)
+vector<ll> dijkstra(vector<vector<pair<int, int>>>& adj, int s) {
+  int n = (int) adj.size();
+  vector<int> vis(n);
+  vl dist(n, -1);
+  pq<pair<ll, int>> pq;
+  pq.emplace(0, s); // (dist, vertex)
 
 
-	while (!pq.empty()) {
-		auto [pd, p] = pq.top(); 
-		pq.pop();
+  while (!pq.empty()) {
+    auto [pd, p] = pq.top(); 
+    pq.pop();
 
-		if (vis[p])
-			continue;
+    if (vis[p])
+      continue;
 
-		vis[p] = 1;
-		dist[p] = pd;
+    vis[p] = 1;
+    dist[p] = pd;
 
-		for (auto& [ch, cw] : adj[p]) {
-			pq.emplace(dist[p] + cw, ch);
-		}
-	}
-
-	print(dist);
+    for (auto& [ch, cw] : adj[p]) {
+      pq.emplace(dist[p] + cw, ch);
+    }
+  }
+  return dist;
 }
