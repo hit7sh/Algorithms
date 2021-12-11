89cd1304569126f25cd74c8824a7f87ddb1e75b9
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-12-11 18:51:02 +0530

    Update Array compression (Coordinate compression).cpp

--- Array compression (Coordinate compression).cpp
+++ Array compression (Coordinate compression).cpp
@@ -2,16 +2,11 @@
 using namespace std;
 
 void compress(vector<int>& A) {
-	vector<int> sorted = A;
-	vector<int> compressed(A.size());
-	sort(sorted.begin(), sorted.end());
-	for (int i = 1; i < (int) A.size(); i++) {
-		if (sorted[i] == sorted[i - 1])
-			compressed[i] = compressed[i - 1];
-		else
-			compressed[i] = compressed[i - 1] + 1;
-	}
-	for (int i = 0; i < (int) A.size(); i++) {
-		A[i] = compressed[lower_bound(sorted.begin(), sorted.end(), A[i]) - sorted.begin()];
-	}
+  vector<int> sorted = A;
+  sort(sorted.begin(), sorted.end());
+  sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+  for (int& x : A) {
+    x = int(lower_bound(sorted.begin(), sorted.end(), x) - sorted.begin());
+  }
 }
+
