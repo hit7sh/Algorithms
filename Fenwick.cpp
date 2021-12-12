4b4ad85d7841aab3a46ad1dd9d77dedafef3698b
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-12-12 22:55:04 +0530

    Update Fenwick.cpp

--- Fenwick.cpp
+++ Fenwick.cpp
@@ -1,27 +1,45 @@
 
 template <typename T>
-class fenwick {
- public:
-  vector<T> fenw;
-  int n;
-
-  fenwick(int _n) : n(_n) {
-    fenw.resize(n);
-  }
-
-  void modify(int x, T v) {
-    while (x < n) {
-      fenw[x] += v;
-      x |= (x + 1);
+struct fenwick {
+    int n;
+    vector<T> node;
+ 
+    fenwick(int _n) : n(_n) {
+        node.resize(n);
     }
-  }
-
-  T get(int x) {
-    T v{};
-    while (x >= 0) {
-      v += fenw[x];
-      x = (x & (x + 1)) - 1;
+ 
+    void add(int x, T v) {
+        while (x < n) {
+            node[x] += v;
+            x |= (x + 1);
+        }
     }
-    return v;
-  }
+ 
+    T get(int x) {  // [0, x]
+        T v = 0;
+        while (x >= 0) {
+            v += node[x];
+            x = (x & (x + 1)) - 1;
+        }
+        return v;
+    }
+ 
+    T get(int x, int y) {  // [x, y]
+        return (get(y) - (x ? get(x - 1) : 0));
+    }
+ 
+    int lower_bound(T v) {
+        int x = 0;
+        int h = 1;
+        while (n >= (h << 1)) {
+            h <<= 1;
+        }
+        for (int k = h; k > 0; k >>= 1) {
+            if (x + k <= n && node[x + k - 1] < v) {
+                v -= node[x + k - 1];
+                x += k;
+            }
+        }
+        return x;
+    }
 };
