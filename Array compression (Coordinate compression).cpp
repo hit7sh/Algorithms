29bc1674b2e52b6b3e9d714a0b99442992023009
Hitesh Kumar Saini <hit7sh@gmail.com>
2022-01-15 23:17:53 +0530

    Update Array compression (Coordinate compression).cpp

--- Array compression (Coordinate compression).cpp
+++ Array compression (Coordinate compression).cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 template<typename T>
-void compress(vector<T>& A) {
+vector<T> compress(vector<T>& A) {
   vector<T> sorted = A;
   sort(sorted.begin(), sorted.end());
   sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
   for (T& x : A) {
     x = int(lower_bound(sorted.begin(), sorted.end(), x) - sorted.begin());
   }
+  return sorted;
 }
