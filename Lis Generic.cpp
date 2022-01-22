e0f6aa48bb543306800739c09bf74c79e12fcbb7
Hitesh Kumar Saini <hit7sh@gmail.com>
2022-01-23 01:34:09 +0530

    Update Lis Generic.cpp

--- Lis Generic.cpp
+++ Lis Generic.cpp
@@ -1,17 +1,17 @@
 #include<bits/stc++.h>
 using namespace std;
 
-vector<int> lis(vector<int> a){
-	int n = (int)a.size();
-	vector<int> r(n, 0);
-	vector<int> stk;
-	for(int i = 0; i < n; i++){
-		int x = lower_bound(stk.begin(), stk.end(), a[i]+1) - stk.begin();
-		r[i] = x;
-		if(x >= (int)stk.size()){
-			stk.push_back(a[i]);
-		}
-		stk[x] = a[i];
-	}
-	return r;
+template<typename T>
+int lis(vector<T>& A) {
+  vector<T> sub;
+  sub.push_back(A[0]);
+  int n = A.size();
+  for (int i = 1; i < n; i++) {
+    if(A[i] > sub.back())
+      sub.emplace_back(A[i]);
+    else
+      *lower_bound(sub.begin(), sub.end(), A[i]) = A[i];
+  }
+  // sub = subsequence itself
+  return (int) sub.size();
 }
