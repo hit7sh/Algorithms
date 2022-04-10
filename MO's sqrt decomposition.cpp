273bf1662c04fc8939f2c456912b51ac6e051a36
Hitesh Kumar Saini <hit7sh@gmail.com>
2022-04-11 00:09:47 +0530

    Update MO's sqrt decomposition.cpp

--- MO's sqrt decomposition.cpp
+++ MO's sqrt decomposition.cpp
@@ -3,57 +3,65 @@
 using namespace std;
 
 
-const int BLOCK = 550, N = 3e5 + 5; // BLOCK = 550 = sqrt(3e5)
-int A[N];
+int main() {
+	
+ 	// take inputs......................
+  int n, q;
+  cin >> n >> q;
+  vector<int> A(n);
+  for (int& x : A) cin >> x;
+	
+  vector<array<int, 3>> Q(q);
+  for (int i = 0; i < q; i++) {
+    int l, r;
+    cin >> l >> r;
+    --l, --r;
+    Q[i] = {l, r, i};
+  }
+	// -------------------------
+	
+	
+	// sort..................................................................	
+  const int BLOCK = sqrt(n);
+  sort(Q.begin(), Q.end(), [&](const array<int, 3>& a, const array<int, 3>& b) {
+    if (a[0] / BLOCK != b[0] / BLOCK) return a[0] / BLOCK < b[0] / BLOCK;
+    return a[1] < b[1];
+  });
+	// -----------------------------------------------------------------------
+	
+  int ans = 0;  // answer of current segment
+  vector<int> mp(n);
 
-struct Query {
-	int L, R, idx;
-	inline bool operator<(const Query& other) const {
-		if (L / BLOCK != other.L / BLOCK)
-			return L / BLOCK < other.L / BLOCK;
-		return R < other.R;
-	}
-} Q[N];
+	
+	
+//-----------------------------------------
+  auto add = [&](int x) {
+	  // add x to the segment
+  };
+  auto remove = [&](int x) {
+	  // remove y from the segment
+  };
+//-----------------------------------------
+	
+  int pL = 0, pR = -1;
+  vector<int> res(q);
+  for (int i = 0; i < q; i++) {
+    auto [L, R, idx] = Q[i];
 
-void add(int pos) {
-	// add A[pos] to range
-}
+    while (pL > L) add(A[--pL]);
+    while (pR < R) add(A[++pR]);
+    while (pL < L) remove(A[pL]), pL++;
+    while (pR > R) remove(A[pR]), pR--;
 
-void remove(int pos) {
-	// remove A[pos] from range
-}
-
-void solve() {
-	// input array and queries and sort queries
-
-	int pL = 0, pR = -1;
-	f0(i, q) {
-		int L = Q[i].L;
-		int R = Q[i].R;
-		
-		while(pL > L)
-		pL-- , add(pL);
-		
-		while(pR < R)
-		pR++ , add(pR);
-		
-		while(pL < L)
-		remove(pL) , pL++;
-		
-		while(pR > R)
-		remove(pR) , pR--;
-		
-		// process answer obtained from query
-		// ans[i] = ...
-	}
-	f0(i, q)
-		cout << ans[i] << ' ';
-}
- 
-
-
-
-
-int main() { 
-     solve();
+    res[idx] = ans;
+  }
+	
+  for (int i = 0; i < q; i++) {
+    cout << res[i] << ' ';
+  }
+	
+	
+	
+	
+	return 0;
 }
